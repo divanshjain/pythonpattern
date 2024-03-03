2aba5944e711b21a23484bcc530a2e4bffdde490
divanshjain <54525608+divanshjain@users.noreply.github.com>
2024-03-03 22:14:56 +0530

    Add files via upload

---
+++ reverstrhomeone.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 20:14:47 2024
+
+@author: Admin
+"""
+
+def reverse(S):
+    str =""
+    for i in s:
+        str=i+str
+    return str
+s= "pawan"
+print("original string is ", end="")
+print(s)
+print("reverse string is ",end="")
+print(reverse(s))
+    
